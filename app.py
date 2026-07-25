1bd43de7ec28f296463a32f8d9883af54b096750
Yara Essam Ellkany <117939379+Yara-Ellkany@users.noreply.github.com>
2026-07-25 19:46:23 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,15 @@
 import streamlit as st
-from transformers import pipeline
+import language_tool_python
 
 st.title(" Smart Keyboard for Kids")
 
-@st.cache_resource
-def load_model():
-    return pipeline(
-        "text2text-generation",
-        model="vennify/t5-base-grammar-correction"
-    )
+tool = language_tool_python.LanguageTool('en-US')
 
-corrector = load_model()
-
-text = st.text_area("Type your sentence")
+text = st.text_area("Type your sentence:")
 
 if st.button("Correct"):
-    if text:
-        result = corrector("grammar: " + text, max_length=100)
-        st.success(result[0]["generated_text"])
+    matches = tool.check(text)
+    corrected = language_tool_python.utils.correct(text, matches)
+
+    st.write("### Corrected Sentence")
+    st.success(corrected)
